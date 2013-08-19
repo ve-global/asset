308309871641d0dbf82898053e473df3ee6513a1
stevewest <uruwolf@gmail.com>
2013-08-19 17:16:06 +0100

    Adds theme crud to core class.

--- src/Ve/Asset/VeAsset.php
+++ src/Ve/Asset/VeAsset.php
@@ -12,6 +12,9 @@
 
 namespace Ve\Asset;
 
+use FuelPHP\Common\Arr;
+use Ve\Asset\Environment\AbstractDriver;
+
 /**
  *
  *
@@ -21,38 +24,217 @@ namespace Ve\Asset;
 class VeAsset
 {
 
+	public static $defaultTheme = 'default';
+
 	/**
 	 * @var DependencyCompilerInterface
 	 */
 	protected $drc;
 
-	protected $defaultTheme;
+	/**
+	 * @var FileCombinerInterface
+	 */
+	protected $fc;
 
-	// Construct with DRC, combination drivers
-	public function __construct(DependencyCompilerInterface $drc)
+	/**
+	 * @var AbstractDriver
+	 */
+	protected $env;
+
+	/**
+	 * @var string
+	 */
+	protected $activeTheme;
+
+	/**
+	 * @var array
+	 */
+	protected $themes = [];
+
+	/**
+	 * @param DependencyCompilerInterface $drc
+	 * @param FileCombinerInterface       $fc
+	 * @param AbstractDriver              $env
+	 */
+	public function __construct(DependencyCompilerInterface $drc, FileCombinerInterface $fc, AbstractDriver $env)
 	{
 		$this->drc = $drc;
-		$this->defaultTheme = 'default';
+		$this->fc = $fc;
+		$this->env = $env;
+
+		// Make sure the default theme exists
+		$this->themes[static::$defaultTheme] = [];
+		$this->activeTheme = static::$defaultTheme;
 	}
 
-	// Add, remove, update theme
-	public function addTheme(){}
+	/**
+	 * Sets the name of the default theme. Call with no parameters to reset to the original default.
+	 *
+	 * @param string $active
+	 *
+	 * @return $this
+	 *
+	 * @throws \InvalidArgumentException
+	 */
+	public function setActiveTheme($active = null)
+	{
+		if ( ! is_string($active))
+		{
+			throw new \InvalidArgumentException('Active theme name must be a string');
+		}
 
-	public function removeTheme(){}
+		if (!array_key_exists($active, $this->themes))
+		{
+			throw new \InvalidArgumentException('Unknown theme '.$active);
+		}
 
-	public function updateTheme(){}
+		$this->activeTheme = $active;
+		return $this;
+	}
+
+	/**
+	 * Gets the name of the default theme
+	 *
+	 * @return string
+	 */
+	public function getActiveTheme()
+	{
+		return $this->activeTheme;
+	}
+
+	/**
+	 * Adds a theme
+	 *
+	 * @param string $name
+	 * @param array  $config
+	 *
+	 * @return $this
+	 *
+	 * @throws \InvalidArgumentException
+	 */
+	public function addTheme($name, array $config)
+	{
+		if ( ! is_string($name))
+		{
+			throw new \InvalidArgumentException('Theme name must be a string');
+		}
+
+		$this->themes[$name] = $config;
+		return $this;
+	}
+
+	/**
+	 * Gets the given theme
+	 *
+	 * @param string|null $name
+	 *
+	 * @return array
+	 *
+	 * @throws \InvalidArgumentException
+	 */
+	public function getTheme($name = null)
+	{
+		if (is_null($name))
+		{
+			$name = $this->getActiveTheme();
+		}
+
+		if ( ! array_key_exists($name, $this->themes))
+		{
+			throw new \InvalidArgumentException('Unknown theme '.$name);
+		}
+
+		return $this->themes[$name];
+	}
+
+	/**
+	 * Removes the given theme
+	 *
+	 * @param $name
+	 *
+	 * @return $this
+	 *
+	 * @throws \InvalidArgumentException
+	 */
+	public function removeTheme($name)
+	{
+		if ($name === $this->getActiveTheme())
+		{
+			throw new \InvalidArgumentException('You cannot remove the active theme');
+		}
+
+		unset($this->themes[$name]);
+		return $this;
+	}
+
+	/**
+	 * Allows a theme value to be set after the theme has been added
+	 *
+	 * @param string      $key   Dot notated key of the config element to change
+	 * @param null|mixed  $value New value to set
+	 * @param null|string $theme Theme to change, defaults to the default theme
+	 *
+	 * @return $this
+	 *
+	 * @throws \InvalidArgumentException
+	 */
+	public function updateTheme($key, $value = null, $theme = null)
+	{
+		if (is_null($theme))
+		{
+			$theme = $this->getActiveTheme();
+		}
+
+		if ( ! array_key_exists($theme, $this->themes))
+		{
+			throw new \InvalidArgumentException('Unknown theme '.$theme);
+		}
+
+		if (is_array($key))
+		{
+			// Merge the arrays
+			$this->themes[$theme] = $key + $this->themes[$theme];
+		}
+		else
+		{
+			// Just update the key
+			Arr::set($this->themes[$theme], $key, $value);
+		}
+
+		return $this;
+	}
 
 	// Add group to theme
-	public function addGroup($group, $theme=null){}
+	public function addGroup($group, $theme = null)
+	{
+		return $this;
+	}
 
 	// remove group from theme
-	public function removeGroup($group){}
+	public function removeGroup($group)
+	{
+		return $this;
+	}
 
 	// update group, change files, disable, etc
-	public function editGroup($group, $property, $value){}
+	public function editGroup($group, $property, $value)
+	{
+		return $this;
+	}
 
 	// Compile js/css into one tag
 	public function css(){}
 
 	public function js(){}
+
+	// Add and get inline js
+	public function inlineJS($js)
+	{
+		return $this;
+	}
+
+	public function renderInlineJS()
+	{
+
+	}
 }
---
+++ tests/Ve/Asset/VeAssetTest.php
@@ -0,0 +1,233 @@
+<?php
+
+/**
+ * This file is part of the VeAsset package.
+ *
+ * Released under the MIT licence.
+ * This file is free to use and reuse as long as the original credits are preserved.
+ *
+ * @license MIT License
+ * @copyright 2013 Ve Interactive
+ */
+
+namespace Ve\Asset;
+
+/**
+ *
+ *
+ * @package Ve\Asset
+ * @author  Ve Interactive PHP Team
+ */
+class VeAssetTest extends \PHPUnit_Framework_TestCase
+{
+
+	/**
+	 * @var VeAsset
+	 */
+	protected $object;
+
+	public function setUp()
+	{
+		$drc = \Mockery::mock('Ve\Asset\DependencyCompilerInterface');
+		$fc = \Mockery::mock('Ve\Asset\FileCombinerInterface');
+		$env = \Mockery::mock('Ve\Asset\Environment\AbstractDriver');
+
+		$this->object = new VeAsset($drc, $fc, $env);
+	}
+
+	/**
+	 * @covers Ve\Asset\VeAsset::getTheme()
+	 */
+	public function testGetActiveTheme()
+	{
+		$this->assertEquals(
+			[],
+			$this->object->getTheme()
+		);
+	}
+
+	/**
+	 * @covers Ve\Asset\VeAsset::addTheme()
+	 * @covers Ve\Asset\VeAsset::getTheme()
+	 */
+	public function testSetGetTheme()
+	{
+		$name = 'test';
+		$config = ['foobar'];
+
+		$this->object->addTheme($name, $config);
+
+		$this->assertEquals(
+			$config,
+			$this->object->getTheme($name)
+		);
+	}
+
+	/**
+	 * @covers            Ve\Asset\VeAsset::getTheme()
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testGetInvalidTheme()
+	{
+		$this->object->getTheme('fake');
+	}
+
+	/**
+	 * @covers            Ve\Asset\VeAsset::getTheme()
+	 * @covers            Ve\Asset\VeAsset::addTheme()
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testRemoveTheme()
+	{
+		$name = 'test';
+		$config = ['foobar'];
+
+		$this->object->addTheme($name, $config);
+		$this->object->removeTheme($name);
+		$this->object->getTheme($name);
+	}
+
+	/**
+	 * @covers Ve\Asset\VeAsset::getTheme()
+	 * @covers Ve\Asset\VeAsset::addTheme()
+	 * @covers Ve\Asset\VeAsset::updateTheme()
+	 */
+	public function testUpdateTheme()
+	{
+		$name = 'test';
+		$config = [
+			'foobar' => [
+				'one' => 123,
+			],
+		];
+
+		$this->object->addTheme($name, $config);
+
+		$this->object->updateTheme('foobar.one', 456, $name);
+
+		$this->assertEquals([
+				'foobar' => [
+					'one' => 456,
+				],
+			],
+			$this->object->getTheme($name)
+		);
+	}
+
+	/**
+	 * Check that the default theme can be changed
+	 * @covers Ve\Asset\VeAsset::getTheme()
+	 * @covers Ve\Asset\VeAsset::updateTheme()
+	 */
+	public function testUpdateActiveTheme()
+	{
+		$this->object->updateTheme('foobar.one', 456);
+
+		$this->assertEquals([
+				'foobar' => [
+					'one' => 456,
+				],
+			],
+			$this->object->getTheme()
+		);
+	}
+
+	/**
+	 * @covers Ve\Asset\VeAsset::getTheme()
+	 * @covers Ve\Asset\VeAsset::addTheme()
+	 * @covers Ve\Asset\VeAsset::updateTheme()
+	 */
+	public function testUpdateAlternateTheme()
+	{
+		$theme = 'foobar';
+
+		$this->object->addTheme($theme, []);
+
+		$this->object->updateTheme('test', 'value', $theme);
+
+		$this->assertEquals(
+			['test' => 'value'],
+			$this->object->getTheme($theme)
+		);
+	}
+
+	/**
+	 * @covers Ve\Asset\VeAsset::getTheme()
+	 * @covers Ve\Asset\VeAsset::updateTheme()
+	 */
+	public function testUpdateWithArray()
+	{
+		$this->object->updateTheme('test', 'should not see');
+
+		$config = ['test' => 'value'];
+		$this->object->updateTheme($config);
+
+		$this->assertEquals(
+			$config,
+			$this->object->getTheme()
+		);
+	}
+
+	/**
+	 * @covers            Ve\Asset\VeAsset::updateTheme()
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testUpdateFakeTheme()
+	{
+		$this->object->updateTheme('key', 'value', 'fake');
+	}
+
+	/**
+	 * Check the default theme cannot be deleted
+	 *
+	 * @covers            Ve\Asset\VeAsset::removeTheme()
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testDeleteActive()
+	{
+		$this->object->removeTheme('default');
+	}
+
+	/**
+	 * Check changing default theme works
+	 *
+	 * @covers Ve\Asset\VeAsset::addTheme()
+	 * @covers Ve\Asset\VeAsset::setActiveTheme()
+	 * @covers Ve\Asset\VeAsset::getActiveTheme()
+	 */
+	public function testChangeDefault()
+	{
+		$name = 'new';
+		$config = ['bla'];
+
+		$this->object->addTheme($name, $config);
+		$this->object->setActiveTheme($name);
+
+		$this->assertEquals(
+			$name,
+			$this->object->getActiveTheme()
+		);
+	}
+
+	/**
+	 * @covers            Ve\Asset\VeAsset::setActiveTheme()
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testChangeInvalidActive()
+	{
+		$name = 'new';
+		$this->object->setActiveTheme($name);
+	}
+
+	/**
+	 * Check that changing the default theme name can only be a string
+	 *
+	 * @covers            Ve\Asset\VeAsset::setActiveTheme()
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testInvalidActive()
+	{
+		$this->object->setActiveTheme(123234);
+	}
+
+}
