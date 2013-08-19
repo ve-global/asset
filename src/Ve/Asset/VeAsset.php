8f66ce496f8b1746df04866abae6669f3441198d
stevewest <uruwolf@gmail.com>
2013-08-19 10:54:00 +0100

    Adds placeholder functions for main class.

--- src/Ve/Asset/VeAsset.php
+++ src/Ve/Asset/VeAsset.php
@@ -21,4 +21,38 @@ namespace Ve\Asset;
 class VeAsset
 {
 
+	/**
+	 * @var DependencyCompilerInterface
+	 */
+	protected $drc;
+
+	protected $defaultTheme;
+
+	// Construct with DRC, combination drivers
+	public function __construct(DependencyCompilerInterface $drc)
+	{
+		$this->drc = $drc;
+		$this->defaultTheme = 'default';
+	}
+
+	// Add, remove, update theme
+	public function addTheme(){}
+
+	public function removeTheme(){}
+
+	public function updateTheme(){}
+
+	// Add group to theme
+	public function addGroup($group, $theme=null){}
+
+	// remove group from theme
+	public function removeGroup($group){}
+
+	// update group, change files, disable, etc
+	public function editGroup($group, $property, $value){}
+
+	// Compile js/css into one tag
+	public function css(){}
+
+	public function js(){}
 }
